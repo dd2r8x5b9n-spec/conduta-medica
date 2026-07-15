8d36c1a25df32781a8de787ffe4d619442a4ba54
dd2r8x5b9n-spec <dd2r8x5b9n@privaterelay.appleid.com>
2026-07-14 21:00:08 -0300

    Add files via upload
    Adicionado suporte offline com Service Worker

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'conduta-medica-v2';
+const urlsToCache = [
+    '/conduta-medica/',
+    '/conduta-medica/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
